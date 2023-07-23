9224b2ea7a5e44f97e17481dab13753dece9d96b
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-07-23 12:40:54 +1200

    Rearrange dexisting files + created titlebar files

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Titlebar from './titlebar/titlebar'
+
+function App() {
+
+  return (
+    <>
+      <Titlebar></Titlebar>
+      <p>Yo this is my website.</p>
+      <h1>ss</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/app/titlebar/Titlebar.tsx
@@ -0,0 +1,7 @@
+import styles from './Titlebar.module.css';
+
+function Titlebar() {
+    return (<h1 className={styles.h1}>Titlehar</h1>)
+}
+
+export default Titlebar;
