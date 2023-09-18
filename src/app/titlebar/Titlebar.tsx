0dc78ab76108e699b6f5ee7dfb6a435224b98eae
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-09-18 22:39:00 +1200

    created titlebar elements

--- src/app/titlebar/Titlebar.tsx
+++ src/app/titlebar/Titlebar.tsx
@@ -1,9 +1,16 @@
 import styles from './Titlebar.module.css';
 
 function Titlebar() {
+
+    
     return (
-    <div>
-        <img className={styles.logo} src="./logo.png"></img>
+    <div className={styles.bar}>
+        <div className={styles.sect1}>
+            <img className={styles.logo} src="./logo.png"></img>
+            <p className={styles.header}>filelocker</p>
+        </div>
+        <button className={styles.home}>Home</button>
+        <button className={styles.project}>Project</button>
     </div>
     )
 }
