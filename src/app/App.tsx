6dd5e2429f3d08f1561455a873341aba8c2319b5
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-09-19 15:39:57 +1200

    Moved Titlebar into App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,44 @@
-import './App.module.css'
-import Titlebar from './titlebar/Titlebar'
+import styles from './App.module.css'
 import Page from './page/Page'
+import {useState} from 'react'
+
 
 function App() {
 
+  const [value, setValue] = useState<number>(0);
+
+  const homeButtonClick = () => {
+    setValue(0)
+  }
+
+  const projectButtonClick = () => {
+    setValue(1)
+  }
+
+
+
   return (
     <>
-      <Titlebar></Titlebar>
-      <Page></Page>
+      <div className={styles.mainPage}>
+        <div className={styles.bar}>
+
+          <div className={styles.sect1}>
+              <p>Count: {value}</p>
+              <img className={styles.logo} src="./logo.png"></img>
+              <p className={styles.header}>filelocker</p>
+
+          </div>
+          <button className={styles.home} onClick={homeButtonClick}>Home</button>
+          <button className={styles.project} onClick={projectButtonClick}>Project</button>
+
+        </div>
+
+        <div className={styles.outerPage}>
+          <div className={styles.innerPage}>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
