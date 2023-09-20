b5f6a9d2a724a509736a24c5b7e6fbd07153ff0d
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-09-20 15:14:20 +1200

    made first page

--- src/app/page/Page.tsx
+++ src/app/page/Page.tsx
@@ -1,10 +1,16 @@
+import styles from './Page.module.css'
+
 function Page() {
 
-        return(
-            <div>
-                <p>hi</p>
-            </div>
-        )   
+    return(
+        <div>
+            <p className={styles.t1}>A basic file encryption/decryption program written in python.</p>
+            <p className={styles.t2}>Secure AES-256 encryption is used to ensure the safety of your data.</p>
+            <img className={styles.img1} src="./file.PNG" alt="Encrypted File"></img>
+            <img className={styles.img2} src="./program.PNG" alt="Program Preview"></img>
+            <div className={styles.filler}></div>
+        </div>
+    )   
 }
 
 
