4703c7aed93a3bee8166b5e25763d9bfa3a951b0
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-09-20 15:14:31 +1200

    started on second page

---
+++ src/app/page2/Page2.tsx
@@ -0,0 +1,11 @@
+import styles from "./Page2.module.css"
+
+function Page2() {
+    return(
+        <div>
+            <p className={styles.t1}>The program can be downloaded at <a href="https://github.com/stpilcher/filelocker/releases">filelocker GitHub Releases</a></p>
+        </div>
+    )
+}
+
+export default Page2;
