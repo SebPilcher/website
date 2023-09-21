41634db78f27a2aa9739259b90f680ce3f4340d2
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-09-21 19:55:16 +1200

    added content to second page

--- src/app/page2/Page2.tsx
+++ src/app/page2/Page2.tsx
@@ -1,9 +1,11 @@
 import styles from "./Page2.module.css"
 
 function Page2() {
+
     return(
         <div>
-            <p className={styles.t1}>The program can be downloaded at <a href="https://github.com/stpilcher/filelocker/releases">filelocker GitHub Releases</a></p>
+            <p className={styles.t1}>The program can be downloaded at <a className={styles.link} href="https://github.com/stpilcher/filelocker/releases" target="_blank" rel="noreferrer noopener">filelocker GitHub Releases</a>.</p>
+            <img className={styles.img1} src="./github.PNG"></img>
         </div>
     )
 }
