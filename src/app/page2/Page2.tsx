68a4a2069a5456224bb05680697dad80e7fafe58
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-09-22 08:06:14 +1200

    added alt for github image

--- src/app/page2/Page2.tsx
+++ src/app/page2/Page2.tsx
@@ -5,7 +5,7 @@ function Page2() {
     return(
         <div>
             <p className={styles.t1}>The program can be downloaded at <a className={styles.link} href="https://github.com/stpilcher/filelocker/releases" target="_blank" rel="noreferrer noopener">filelocker GitHub Releases</a>.</p>
-            <img className={styles.img1} src="./github.PNG"></img>
+            <img className={styles.img1} src="./github.PNG" alt="github img"></img>
         </div>
     )
 }
